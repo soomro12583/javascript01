c3570a3fbb7f407f6306b7189430f546804a0853
soomro12583 <nadeemkhanasf909@gmail.com>
2023-10-13 16:41:36 +0500

    changes

--- app.js
+++ app.js
@@ -1,105 +1,174 @@
 //chapter No.01
+
 // Question No.1
-//alert ("Error! please enter a valid password");
+alert ("Error! please enter a valid password");
 
 //Question No.2
-//alert ("Welcome to JS land.... \n Happy Coding!"); 
+alert ("Welcome to JS land.... \n Happy Coding!"); 
 
 //Question No.3
-//alert ("Welcome to JS Land...");
+alert ("Welcome to JS Land...");
 
 //Question No.4
-//alert ("Happy Coding! \n \n Prevent this page from creating aditional dailogs"); 
+alert ("Happy Coding! \n \n Prevent this page from creating aditional dailogs"); 
 
 //Question No.5
-//alert ("Hello... I can run JS Through my web browser's console");
+alert ("Hello... I can run JS Through my web browser's console");
 
 //Chapter No.02
+
 //Question No.1
-//var name="Muhammad Nadeem";
-//alert (name)
+var name="Muhammad Nadeem";
+alert (name)
 
 //Question No.2
-//var myAge="15 year old";
-//alert (myAge)
+var myAge="15 year old";
+alert (myAge)
 
 //Question No.3
 var coment="Certified mobile application development";
-//alert (coment)
+alert (coment)
 
 //Question No.4
 var pizza="PIZZA \n PIZZ \n PIZZ \n PIZ \n PI \n P";
-//alert (pizza)
+alert (pizza)
 
 //Question No.5
 var email="My email address is example@example.com";
-//alert (email)
+alert (email)
 
 //Question No.6
 var book="I am trying to learning from the book";
-//alert (book)
+alert (book)
 
 //Question No.7
-<<<<<<< HEAD
 var browser="Yah! I can write HTML content through Javascript";
-//document.write (browser)
+document.write (browser)
 
 //Question No.8
 var content= "“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”";
-//alert (content)
+alert (content)
 
 //Question No.9
 var first="Ghous ";
    last="Ahmed";
-//alert (first +" "+ last)
-=======
+alert (first +" "+ last)
+
 var browser="";
-//alert (browser)
+alert (browser)
 
 //Question No.8
 var content="“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”";
-//alert (content)
+alert (content)
 
 //Question No.9
 var name ="Ghous";
-//alert(name)
->>>>>>> 2150ca0b57aad8e8ac0dc621885b5f19c4c4f332
+alert(name)
 
 //Question No.10
 var first="Ghous ";
 var   last="Ahmed";
-//alert (first +" "+ last)
+alert (first +" "+ last)
 
-//Question No.11
-var phone= 
-alert (phone)
 
 //chapter No.3
+
 //Question No.1
 var age = "I am 15 years old";
-//alert (age)
+alert (age)
 
 //Question No.2
 var visit = "You have visited this site 14 time";
-//alert (visit)
+alert (visit)
 
 //Question No.3
 var myBirth = "My Birth is 1990 <br><br> Date type of my declared variable is number";
-//document.write (myBirth)
+document.write (myBirth)
 
 //Question No.4
 var johnDoe = "<b>John Doe</b> orderd <b>5 T-shirts</b>(s) on XYZ clothing store"
-//document.write(johnDoe)
+document.write(johnDoe)
 
 //chapter N0.4
+
 //Question No.1
 var rules = "<h1>Rules of naming JS variables</h1><br><br><br> variable names can only contains , nubers $ , and _. For example : $my_1st variable<br>variable must bigun with a letter, $ or _. For example : $name,_name or name<br> variable names are case sensitive<br> veriable names should not be Js keywords";
 document.write (rules)
 
+//prectice
+
+var weight = 27;
+//document.write( weight + 7)
+
+var orignalNum = 23;
+var newNum = orignalNum + 7;
+//alert(newNum)
+
+var originalNum = 90;
+originalNum = originalNum + 10;
+//alert(originalNum)
+
+var originalNum = "23";
+var newNum = originalNum + 7;
+//document.write (newNum)
+
+var caseQty = "144";
+//alert(caseQty);
+
+var rose = "florida";
+//document.write(rose);
+
+var popularNumber = 2 + 2;
+//alert(popularNumber)
+
+var popularNumber = 12 - 24;
+//alert(popularNumber)
+
+var popularNumber = 3 * 12;
+//alert(popularNumber)
+
+//alert(2+2);
+
+var num = 10;
+var popularNumber = num + 200;
+//alert(popularNumber)
 
 
+var num = 10;
+var anotherNum = 1;
+var popularNumber = num + anotherNum;
+//document.write(popularNumber)
 
+var whatsLeftOver = 9 % 3;
+//alert(whatsLeftOver);
 
+var num = 1;
+var newNum = num++;
+//alert(newNum);
+
+var num = 1;
+var newNum = ++num;
+//alert(newNum);
+
+var num = 1;
+var newNum = num--;
+//alert(newNum);
+
+var num = 1;
+var newNum = --num;
+//alert(newNum);
+
+//alert("Thanks, " + "susan" + "!");
+
+var messege = "Thanks,";
+var userName = "Susan";
+var banger = "!";
+var customMess = messege + userName + banger;
+//alert(customMess);
+
+//alert ("2" + "2");
+
+//alert ("2 plus 2 equals" + 2 + 2);
 
 
 
